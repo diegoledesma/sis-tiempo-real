6b805666353dfd02943cdb9f163a98d9415b9974
Diego Ledesma <diego.ledesma@changepoint.com>
2017-12-12 00:12:36 -0300

    Update contact form and color selection

---
+++ js/contacto.js
@@ -0,0 +1,27 @@
+function validarFormulario() {
+    var textoNombreUsuario = $('#nombre_usuario').val(),
+        textoEmail = $('#email').val(),
+        textoConsulta = $('#consulta').val();
+    
+    return  textoNombreUsuario &&
+            textoEmail &&
+            textoConsulta
+}
+
+function enviarFormulario(event) {
+
+    var $mensajeError = $('.msj-error-validacion'),
+        $mensajeOk = $('.msj-enviado');
+
+    $mensajeError.hide();
+    $mensajeOk.hide();
+
+    if (!validarFormulario()) {
+        $mensajeError.show();
+        event.stopPropagation();
+    }
+
+    $mensajeOk.show();
+    event.preventDefault();
+}
+
