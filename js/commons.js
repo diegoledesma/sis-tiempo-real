abd5e142d0c6c51eca1971691468adfc25d02d58
Diego Ledesma <diego.ledesma@changepoint.com>
2017-12-11 21:11:25 -0300

    cleanup. add encuesta pdf. set behavior to change color. set behavior to change font size

--- js/commons.js
+++
@@ -1,6 +0,0 @@
-function resize (){
-	var valor_input = $('#text_resize').val();
-	if(!isNaN(valor_input)){
-		document.body.style.fontSize = valor_input+"px";
-	}
-}
---
+++ js/comun.js
@@ -0,0 +1,19 @@
+function aplicarColor(nombreColor) {
+    $('body').css({'background-color': nombreColor});
+}
+
+function validarTamanoFuente(tamanoFuente) {
+    if (tamanoFuente < 10 || tamanoFuente > 25) {
+        alert('Debe ingresar un tamaño de letra de entre 10px y 25px');
+        return false;
+    }
+
+    return true;
+}
+
+function aplicarTamanoFuente() {
+    var valorTamanoFuente = $('.cambio-letra input').val();
+    if (validarTamanoFuente(valorTamanoFuente)) {
+        $('body').css({'font-size': `${valorTamanoFuente}px` })
+    }
+}
